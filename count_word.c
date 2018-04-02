821abd48ec297790d14d8050c3de8d216f0e1b64
arkango <asaracino@aryma.it>
2018-04-02 18:55:09 +0200

    programma che conta le parole

--- count_word.c
+++ count_word.c
@@ -4,23 +4,36 @@
 */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int word_counter(char *inp);
 
 int main()
 {
-    char inputString[101];
-    char *token;
 
-    printf("Inserisci la tua stringa");
-    gets(inputString);
+    int count;
+   
+    char inp[101] = "";
+    fgets(inp, 101, stdin);
+    printf("\n %d",word_counter(inp)); 
 
-    token = strtok(inputString, ' ');
-
-    while (token != NULL)
-    {
-        printf(" %s\n", token);
-
-        token = strtok(NULL, ' ');
-    }
 
     return 0;
+}
+int word_counter(char *inp)
+{
+    char *inputString, *token, *toFree;
+    int counter = 0;
+
+    inputString = toFree = strdup(inp);
+
+    while ((token = strsep(&inputString, " ")) != NULL)
+    {
+        
+        if(token != " ") counter++;
+    }
+    free(toFree);//libero memoria
+    //printf("\n %d",counter);
+    return counter;
 }
