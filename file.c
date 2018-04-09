d5560c36823617bbdb7dcca88cae2a257d9cb675
arkango <asaracino@aryma.it>
2018-04-09 12:20:17 +0200

    peggio di un virus

--- file.c
+++ file.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 int main()
 {
     FILE *file;
@@ -11,18 +10,31 @@ int main()
     char name[30];
     double balance;
 
-    file = fopen("test.dat","w");
+    file = fopen("test.dat", "w+");
 
-    if(file == NULL){
+    if (file == NULL)
+    {
         puts("errore nell'apertura del file");
-    }else{
-        printf("inserisci numero , nome, saldo");
+    }
+    else
+    {
+        printf("inserisci numero , nome, saldo \n");
         printf("enter EOF to enf input \n");
         printf("? \n");
-        scanf("%d%s%lf");
-    }
+        scanf("%d%s%lf", &account, name, &balance);
 
-    if(!fclose(file)){ puts("non chiuso"); /*printf("valore fclose %d",fclose(file));*/ }
+        while (!feof(stdin))
+        {
+            fprintf(file, "%d %s %.2f\n", account, name, balance);
+            printf("? \n");
+            //scanf("%d%s%lf", &account, name, &balance);
+        }
+
+        if (!fclose(file))
+        {
+            puts("non chiuso"); /*printf("valore fclose %d",fclose(file));*/
+        }
+    }
 
     return 0;
 }
